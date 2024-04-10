d331b0244a5ea116ca1f391a19219de0d1648542
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-10 12:35:09 -0700

    client side validation for creating new issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,16 +7,18 @@ import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod'
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
 
     const router = useRouter()
-    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    })
     const [error, setError] = useState("")
 
     return (
@@ -34,10 +36,12 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder='Title' {...register('title')} />
                 </TextField.Root>
+                {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
                 <Controller name="description" control={control} render={({ field }) =>
                     <SimpleMDE placeholder='Description' {...field} />
                 }>
                 </Controller>
+                {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
                 <Button>Submit New Issue</Button>
                 {error && (
                     <Callout.Root color="red">
