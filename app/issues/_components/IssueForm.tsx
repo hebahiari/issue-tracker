4fdf3c6c2b956d7e2ff1f8750e20f1ba6d69f901
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-12 18:56:21 -0700

    Revert "fixed issue with simpleMDE"
    This reverts commit 9ef2d8eb7281039a238ebee0ebfecdbaec06fd47.

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,13 +8,18 @@ import { Issue } from '@prisma/client';
 import { Button, Callout, Heading, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { IoIosArrowBack } from 'react-icons/io';
 import { z } from 'zod';
-import SimpleMDE from "react-simplemde-editor";
+
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'),
+    { ssr: false }
+)
 
 type IssueFormData = z.infer<typeof issueSchema>
 
