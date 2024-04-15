7cd20eb10dc1c8e15df81de327542f41c9178377
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-14 20:21:32 -0700

    added login and logout buttons

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,11 +5,14 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { GrBug } from "react-icons/gr"
 import classNames from 'classnames'
+import { useSession } from 'next-auth/react'
+import { Box } from '@radix-ui/themes'
 
 
 const Navbar = () => {
 
     const currentPath = usePathname()
+    const { status, data: session } = useSession()
 
     const links = [
         { label: 'Dashboard', href: "/" },
@@ -18,16 +21,26 @@ const Navbar = () => {
 
     return (
         <nav className='flex items-center border-b space-x-6 mb-5 px-5 h-14 '>
-            <Link href="/"><GrBug />
-            </Link>
+            <Link href="/"><GrBug /></Link>
             <ul className='flex space-x-6'>
-                {links.map((link) => <li key={link.href}><Link className={classNames({
-                    'text-zinc-900': link.href === currentPath,
-                    'text-zinc-500': link.href !== currentPath,
-                    'hover:text-zinc-800 transition-colors': true
-                })} href={link.href}>{link.label}</Link></li>
-                )}            </ul>
-        </nav>)
+                {links.map((link) => <li key={link.href}>
+                    <Link className={classNames({
+                        'text-zinc-900': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-zinc-800 transition-colors': true
+                    })} href={link.href}>{link.label}
+                    </Link>
+                </li>
+                )}
+            </ul>
+            <Box>
+                {status === 'authenticated' &&
+                    (<Link href='/api/auth/signout'>Log out</Link>)}
+                {status === 'unauthenticated' &&
+                    (<Link href='/api/auth/signin'>Log in</Link>)}
+            </Box>
+        </nav>
+    )
 }
 
 export default Navbar
