f406ea186bb3495856cba21fbfa055af3a46ff84
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-18 21:16:38 -0700

    built issues chart component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Pagination from "./components/Pagination";
 import IssuesSummary from "./IssuesSummary";
 import RecentIssues from "./RecentIssues";
 import { Flex } from "@radix-ui/themes";
+import IssuesChart from "./IssuesChart";
 
 export default async function Home() {
 
@@ -13,6 +14,7 @@ export default async function Home() {
   return (
     <Flex direction='column' gap='3'>
       <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
+      <IssuesChart open={open} inProgress={inProgress} closed={closed} />
       <RecentIssues />
     </Flex>
   )
